76102e248d53f3bca74825292daa194937e87908
Ahad1713 <khanazan761@gmail.com>
2023-07-11 23:01:28 +0530

    :sparkles: Experience added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,17 @@
 import { Html, useProgress } from "@react-three/drei";
+import React from "react";
 
-const Loader = () => {
+const CanvasLoader = () => {
   const { progress } = useProgress();
   
   return (
-    <div>Loader</div>
-  )
-}
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="text-14 text-[#f1f1f1] font-[800] mt-40">
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
